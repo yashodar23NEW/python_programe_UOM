65924cc0730c6a40e4ba0c84a00eccfcd2efe59e
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-28 18:37:12 +0530

    Update 4.2-loops and iterations.py

--- 4.2-loops and iterations.py
+++ 4.2-loops and iterations.py
@@ -1,5 +1,24 @@
+#01
+
 val = int(input(''))
 for x in range (0, val):
     for y in range(0,val):
       print('*',end='')
     print('')
+
+
+#02
+
+i = int(input())
+j = 2
+while j * j <= i:
+    if not(i % j):
+        print("not a prime")
+        break
+    j += 1  # Increment j by 1, not by 2
+else:
+    print("prime")
+
+
+
+
