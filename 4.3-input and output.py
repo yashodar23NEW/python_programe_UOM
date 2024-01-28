f5a86bea4d776be5aeb96d1cabf3c509c28526c6
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-28 18:41:41 +0530

    Create 4.3-input and output.py

---
+++ 4.3-input and output.py
@@ -0,0 +1,11 @@
+def get_input():
+    # Only edit the code segment between the dashes
+    
+    # ------------------------------------
+    
+    a = int (input("Enter number one: "))
+    b =int( input("Enter number two: "))
+    
+    # ------------------------------------
+    
+    print (a+b)
