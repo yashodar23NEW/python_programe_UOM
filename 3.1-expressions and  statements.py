0bcf2eff1ea3c48c7e0da6c6cdc1e52a3809e7e3
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-29 14:42:07 +0530

    Create 3.1-expressions and  statements.py

---
+++ 3.1-expressions and  statements.py
@@ -0,0 +1,10 @@
+#take the user's name as input
+name = input("Enter your name: ")
+print(name)
+
+#the variable that includes the welcome message is already provided.
+#Please complete this part using the knowledge obtained in this lesson.
+greeting = "Welcome "+name
+
+#print the welcome message
+print(greeting)
