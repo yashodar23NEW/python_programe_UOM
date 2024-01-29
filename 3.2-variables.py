fe80dc2715eb89313e45d32f337f50d6237e0b59
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-29 14:43:34 +0530

    Create 3.2-variables.py

---
+++ 3.2-variables.py
@@ -0,0 +1,21 @@
+A=112.3
+
+B=43
+
+C="Apple"
+
+D=False
+
+#Write the code to output the data type of A 
+print(type(A))
+
+#Write the code to output the data type of B
+print(type(B))
+
+
+#Write the code to output the data type of C
+print(type(C))
+
+
+#Write the code to output the data type of D
+print(type(D))
